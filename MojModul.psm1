e80999d24205189ee523443441549a4a1b5e7e9e
bielawb <bartek.bielawski@live.com>
2013-09-20 18:32:27 +0200

    Przebudowa modulu by uwzglednic formatki: -- zmiana w Get-Data by tworzyc obiekt o odpowiednim typie -- zmiana w MojModul.psd1 by dodac FormatsToProcess -- uzpelnienie pliku MojModul.format.ps1xml trescia

--- MojModul.psm1
+++ MojModul.psm1
@@ -1,8 +1,18 @@
-param ($wersja)
+param (
+    $wersja = [System.Version]'0.1.0.0'
+)
 
 function Get-Data {
+[OutputType('MojModul.Test')]
 param (
 	$wersja = $script:wersja
 )
-    "Wersja wynosi: {0}" -f $wersja
+    New-Object PSObject -Property @{
+        Wersja = $wersja
+        User = $env:USERNAME
+        Data = Get-Date -Format g
+    } | Foreach-Object {
+        $_.PSTypeNames.Insert(0,'MojModul.Test')
+        $_
+    }
 }
